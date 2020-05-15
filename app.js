1218adab2ebc524ea6db93c607981682804e5897
johngeoffrey <geoffjnewman@gmail.com>
2020-05-15 10:27:57 +0100

    Add onchange method

--- app.js
+++ app.js
@@ -5,11 +5,17 @@
     el: "#vue-root",
     data: {
       sizes: window.Inventory.allSizes,
-      selectedSize: 9
+      selectedSize: 9,
+      colors: window.Inventory.allColors,
     },
     computed: {
       sizeClass: function() {
         return 'product-size--' + this.selectedSize.toString().replace('.', '_');
+      }
+    },
+    methods: {
+      updateColorsBySize: function(evt){
+        this.colors = window.Inventory.bySize[evt.target.value];
       }
     }
   })
