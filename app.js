a3845bff5aa3488e8a49335ba4b230e1ed5cbd73
johngeoffrey <geoffjnewman@gmail.com>
2020-05-15 10:22:10 +0100

    Add computed property

--- app.js
+++ app.js
@@ -6,6 +6,11 @@
     data: {
       sizes: window.Inventory.allSizes,
       selectedSize: 9
+    },
+    computed: {
+      sizeClass: function() {
+        return 'product-size--' + this.selectedSize.toString().replace('.', '_');
+      }
     }
   })
 
